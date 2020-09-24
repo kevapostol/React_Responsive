04430e6ef2bddf7eb418da94089421eb9837b558
Kevin Apostol <apostolkevin@gmail.com>
2020-09-23 20:15:19 -0700

    Starter file

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
